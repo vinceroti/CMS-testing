b2519d7861e6ca37b5af0d4b8b40d2f467cf1b08
Vince Roti <vince.roti@gmail.com>
2018-04-23 10:22:11 -0700

    first init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import Contentstack from 'contentstack'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created () {
+    this.msg = Contentstack.Stack('blt327d6e6ccdef50f9', 'blt98e152db08d79009', 'Ride Snowboards')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
