1428c492fc46d4751f40cc375c589a2bd217f5d1
Vince Roti <vince.roti@gmail.com>
2018-06-14 14:56:23 -0700

    cms env

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,10 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+require('dotenv').config()
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  CONTENTSTACK_API: JSON.stringify(process.env.CONTENTSTACK_API),
+  CONTENTSTACK_TOKEN: JSON.stringify(process.env.CONTENTSTACK_TOKEN)
 })
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,58 @@
 <template>
   <div class="hello">
-    {{ msg }}
+    <h1></h1>
+    <p v-html="bio"/>
+    <p v-for="img in imgs">
+      <img v-bind:src="img.url" />
+    </p>
   </div>
 </template>
 
 <script>
 import Contentstack from 'contentstack'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      bio: 'Loading...',
+      imgs: '',
+      entry: ''
     }
   },
   created () {
-    this.msg = Contentstack.Stack('blt327d6e6ccdef50f9', 'blt98e152db08d79009', 'Ride Snowboards')
+    const self = this
+    const Stack = Contentstack.Stack(process.env.CONTENTSTACK_API, process.env.CONTENTSTACK_TOKEN, 'staging')
+
+    const Query1 = Stack.ContentType('team_members').Entry('blt3ec29586cf35b4d6')
+    const Query2 = Stack.ContentType('products').Query().descending('updated_at').limit(1).toJSON().find()
+
+    Query2.then(function success (query) {
+      console.log(query)
+    }, function error (err) {
+      console.log(err)
+    })
+
+    Query1.fetch().then(function success (entry) {
+
+      entry = entry.toJSON()
+      console.log(entry)
+      self.entry = entry
+      self.bio = entry.bio
+      self.imgs = entry.lifestyle_photos
+
+      console.log(entry.search('Welcome to demo'))
+
+    }, function error (err) {
+      console.log(err)
+    })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  img {
+    width: 500px;
+  }
 </style>
